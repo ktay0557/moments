d9aaa2fd310ce300079821f0b37a68c925a78621
Kayleigh Taylor <ktay0557@gmail.com>
2024-03-31 15:09:51 +0000

    Add functionality to the Sign In form

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
@@ -8,21 +8,41 @@ import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
 
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import axios from "axios";
 
 function SignInForm() {
-    //   Add your component logic here
+    const [signInData, setSignInData] = useState({
+        username: '', 
+        password: '',
+    });
+    const { username, password } = signInData;
 
+    const history = useHistory();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post('/dj-rest-auth/registration/', signInData);
+            history.push("/");
+        } catch(err) {
+        }
+    };
+    const handleChange = (event) => {
+        setSignInData({
+            ...signInData,
+            [event.target.name]: event.target.value,
+        });
+    };
     return (
         <Row className={styles.Row}>
             <Col className="my-auto p-0 p-md-2" md={6}>
                 <Container className={`${appStyles.Content} p-4 `}>
                     <h1 className={styles.Header}>sign in</h1>
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group controlId="username">
                             <Form.Label className="d-none">Username</Form.Label>
                             <Form.Control
@@ -30,15 +50,10 @@ function SignInForm() {
                                 type="text"
                                 placeholder="Username"
                                 name="username"
-                                // value={username}
-                                // onChange={handleChange}
+                                value={username}
+                                onChange={handleChange}
                             />
                         </Form.Group>
-                        {/* {errors.username?.map((message, idx) =>
-                            <Alert variant="warning" key={idx}>
-                                {message}
-                            </Alert>
-                        )} */}
 
                         <Form.Group controlId="password">
                             <Form.Label className="d-none">Password</Form.Label>
@@ -47,29 +62,17 @@ function SignInForm() {
                                 type="password"
                                 placeholder="Password"
                                 name="password"
-                                // value={password}
-                                // onChange={handleChange}
+                                value={password}
+                                onChange={handleChange}
                             />
                         </Form.Group>
-                        {/* {errors.password?.map((message, idx) =>
-                            <Alert variant="warning" key={idx}>
-                                {message}
-                            </Alert>
-                        )} */}
-
                         <Button
                             className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
                             type="submit"
                         >
                             Sign In
                         </Button>
-                        {/* {errors.non_field_errors?.map((message, idx) =>
-                            <Alert variant="warning" key={idx} className="mt-3">
-                                {message}
-                            </Alert>
-                        )} */}
                     </Form>
-
                 </Container>
                 <Container className={`mt-3 ${appStyles.Content}`}>
                     <Link className={styles.Link} to="/signup">
