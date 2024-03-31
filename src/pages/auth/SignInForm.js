55963e01b26a01ecd3f92ac145ffc3daaca334c7
Kayleigh Taylor <ktay0557@gmail.com>
2024-03-31 14:57:55 +0000

    Create Sign In form

---
+++ src/pages/auth/SignInForm.js
@@ -0,0 +1,93 @@
+import React from "react";
+
+import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+
+import { Link } from "react-router-dom";
+
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+
+function SignInForm() {
+    //   Add your component logic here
+
+    return (
+        <Row className={styles.Row}>
+            <Col className="my-auto p-0 p-md-2" md={6}>
+                <Container className={`${appStyles.Content} p-4 `}>
+                    <h1 className={styles.Header}>sign in</h1>
+                    <Form>
+                        <Form.Group controlId="username">
+                            <Form.Label className="d-none">Username</Form.Label>
+                            <Form.Control
+                                className={styles.Input}
+                                type="text"
+                                placeholder="Username"
+                                name="username"
+                                // value={username}
+                                // onChange={handleChange}
+                            />
+                        </Form.Group>
+                        {/* {errors.username?.map((message, idx) =>
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        )} */}
+
+                        <Form.Group controlId="password">
+                            <Form.Label className="d-none">Password</Form.Label>
+                            <Form.Control
+                                className={styles.Input}
+                                type="password"
+                                placeholder="Password"
+                                name="password"
+                                // value={password}
+                                // onChange={handleChange}
+                            />
+                        </Form.Group>
+                        {/* {errors.password?.map((message, idx) =>
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        )} */}
+
+                        <Button
+                            className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+                            type="submit"
+                        >
+                            Sign In
+                        </Button>
+                        {/* {errors.non_field_errors?.map((message, idx) =>
+                            <Alert variant="warning" key={idx} className="mt-3">
+                                {message}
+                            </Alert>
+                        )} */}
+                    </Form>
+
+                </Container>
+                <Container className={`mt-3 ${appStyles.Content}`}>
+                    <Link className={styles.Link} to="/signup">
+                        Don't have an account? <span>Sign up now!</span>
+                    </Link>
+                </Container>
+            </Col>
+            <Col
+                md={6}
+                className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
+            >
+                <Image
+                    className={`${appStyles.FillerImage}`}
+                    src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero.jpg"}
+                />
+            </Col>
+        </Row>
+    );
+}
+
+export default SignInForm;
