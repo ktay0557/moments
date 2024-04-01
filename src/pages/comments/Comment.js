1eba0ea9a48d478b6b6710b97b1b98697eed192e
Kayleigh Taylor <ktay0557@gmail.com>
2024-04-01 16:13:02 +0000

    Add functionality to dropdown menu on comments to allow for editing

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../../styles/Comment.module.css';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import { Media } from 'react-bootstrap';
@@ -6,6 +6,7 @@ import Avatar from '../../components/Avatar';
 import { Link } from 'react-router-dom';
 import { axiosRes } from '../../api/axiosDefaults';
 import { MoreDropdown } from '../../components/MoreDropdown';
+import CommentEditForm from './CommentEditForm';
 
 const Comment = (props) => {
     const {
@@ -19,6 +20,8 @@ const Comment = (props) => {
         setComments,
     } = props;
 
+    const [showEditForm, setShowEditForm] = useState(false);
+
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
 
@@ -30,7 +33,7 @@ const Comment = (props) => {
                     ...prevPost.results[0],
                     comments_count: prevPost.results[0].comments_count - 1,
                 },
-            ],
+                ],
             }));
 
             setComments(prevComments => ({
@@ -44,25 +47,36 @@ const Comment = (props) => {
 
 
     return (
-        <div>
+        <>
             <hr />
             <Media>
                 <Link to={`/profiles/${profile_id}`}>
                     <Avatar src={profile_image} />
                 </Link>
-                <Media.Body className='align-self-center ml-2'>
+                <Media.Body className="align-self-center ml-2">
                     <span className={styles.Owner}>{owner}</span>
                     <span className={styles.Date}>{updated_at}</span>
-                    <p>{content}</p>
+                    {showEditForm ? (
+                        <CommentEditForm
+                            id={id}
+                            profile_id={profile_id}
+                            content={content}
+                            profileImage={profile_image}
+                            setComments={setComments}
+                            setShowEditForm={setShowEditForm}
+                        />
+                    ) : (
+                        <p>{content}</p>
+                    )}
                 </Media.Body>
-                {is_owner && (
+                {is_owner && !showEditForm && (
                     <MoreDropdown
-                        // handleEdit={handleEdit}
+                        handleEdit={() => setShowEditForm(true)}
                         handleDelete={handleDelete}
                     />
                 )}
             </Media>
-        </div>
+        </>
     );
 };
 
