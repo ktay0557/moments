47aa3aedcb0956fd201a6e3c79f2519b123d4681
Kayleigh Taylor <ktay0557@gmail.com>
2024-04-01 16:07:03 +0000

    Add Comment component dropdown menu to allow users to delete their comments

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import styles from '../../styles/Comment.module.css';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import { Media } from 'react-bootstrap';
 import Avatar from '../../components/Avatar';
 import { Link } from 'react-router-dom';
+import { axiosRes } from '../../api/axiosDefaults';
+import { MoreDropdown } from '../../components/MoreDropdown';
 
 const Comment = (props) => {
     const {
@@ -10,23 +13,55 @@ const Comment = (props) => {
         profile_image,
         owner,
         updated_at,
-        content
+        content,
+        id,
+        setPost,
+        setComments,
     } = props;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+
+    const handleDelete = async () => {
+        try {
+            await axiosRes.delete(`/comments/${id}/`);
+            setPost((prevPost) => ({
+                results: [{
+                    ...prevPost.results[0],
+                    comments_count: prevPost.results[0].comments_count - 1,
+                },
+            ],
+            }));
+
+            setComments(prevComments => ({
+                ...prevComments,
+                results: prevComments.results.filter((comment) => comment.id !== id),
+            }));
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
 
     return (
         <div>
-        <hr />
-        <Media>
-            <Link to={`/profiles/${profile_id}`}>
-                <Avatar src={profile_image} />
-            </Link>
-            <Media.Body className='align-self-center ml-2'>
-                <span className={styles.Owner}>{owner}</span>
-                <span className={styles.Date}>{updated_at}</span>
-                <p>{content}</p>
-            </Media.Body>
-        </Media>
+            <hr />
+            <Media>
+                <Link to={`/profiles/${profile_id}`}>
+                    <Avatar src={profile_image} />
+                </Link>
+                <Media.Body className='align-self-center ml-2'>
+                    <span className={styles.Owner}>{owner}</span>
+                    <span className={styles.Date}>{updated_at}</span>
+                    <p>{content}</p>
+                </Media.Body>
+                {is_owner && (
+                    <MoreDropdown
+                        // handleEdit={handleEdit}
+                        handleDelete={handleDelete}
+                    />
+                )}
+            </Media>
         </div>
     );
 };
