c0f80ba3d219c107ab337c99605a076cd3f8f9cd
Kayleigh Taylor <ktay0557@gmail.com>
2024-04-01 15:04:46 +0000

    Add CommentCreateForm to allow logged in users to comment on posts

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -8,20 +8,27 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function PostPage() {
     const { id } = useParams();
     const [post, setPost] = useState({ results: [] });
 
+    const currentUser = useCurrentUser();
+    const profile_image = currentUser?.profile_image;
+    const [comments, setComments] = useState({ results: [] });
+
+
     useEffect(() => {
         const handleMount = async () => {
             try {
-                const [{data: post}] = await Promise.all([
+                const [{ data: post }] = await Promise.all([
                     axiosReq.get(`/posts/${id}`),
                 ]);
-                setPost({results: [post]});
+                setPost({ results: [post] });
                 console.log(post);
-            } catch(err){
+            } catch (err) {
                 console.log(err);
             }
         };
@@ -36,7 +43,17 @@ function PostPage() {
                 <p>Popular profiles for mobile</p>
                 <Post {...post.results[0]} setPosts={setPost} postPage />
                 <Container className={appStyles.Content}>
-                    Comments
+                    {currentUser ? (
+                        <CommentCreateForm
+                            profile_id={currentUser.profile_id}
+                            profileImage={profile_image}
+                            post={id}
+                            setPost={setPost}
+                            setComments={setComments}
+                        />
+                    ) : comments.results.length ? (
+                        "Comments"
+                    ) : null}
                 </Container>
             </Col>
             <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
