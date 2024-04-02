bfc8aac010714608ba636dfff326caccaf84b787
Kayleigh Taylor <ktay0557@gmail.com>
2024-04-02 17:48:44 +0000

    Add functionality to allow users to update profiles with images, usernames and passwords

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 import PostEditForm from "./pages/posts/PostEditForm";
 import ProfilePage from "./pages/profiles/ProfilePage";
+import UsernameForm from "./pages/profiles/UsernameForm";
+import UserPasswordForm from "./pages/profiles/UserPasswordForm";
+import ProfileEditForm from "./pages/profiles/ProfileEditForm";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -21,32 +24,32 @@ function App() {
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route 
-            exact 
-            path="/" 
+          <Route
+            exact
+            path="/"
             render={() => (
-              <PostsPage message="No results found. Adjust the search keyword."/>
-            )} 
+              <PostsPage message="No results found. Adjust the search keyword." />
+            )}
           />
-          <Route 
-            exact 
-            path="/feed" 
+          <Route
+            exact
+            path="/feed"
             render={() => (
-              <PostsPage 
+              <PostsPage
                 message="No results found. Adjust the search keyword or follow a user."
                 filter={`owner__followed__owner__profile=${profile_id}&`}
               />
-            )} 
+            )}
           />
-          <Route 
-            exact 
-            path="/liked" 
+          <Route
+            exact
+            path="/liked"
             render={() => (
-              <PostsPage 
+              <PostsPage
                 message="No results found. Adjust the search keyword or like a post."
                 filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
               />
-            )} 
+            )}
           />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
@@ -54,6 +57,9 @@ function App() {
           <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
           <Route exact path="/profiles/:id/" render={() => <ProfilePage />} />
+          <Route exact path="/profiles/:id/edit/username" render={() => <UsernameForm />} />
+          <Route exact path="/profiles/:id/edit/password" render={() => <UserPasswordForm />} />
+          <Route exact path="/profiles/:id/edit" render={() => <ProfileEditForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
