383d0691d70f055829a5323915c5233086395e11
Kayleigh Taylor <ktay0557@gmail.com>
2024-04-02 17:10:10 +0000

    Add follow functionality for profiles

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -23,13 +23,17 @@ import NoResults from "../../assets/no-results.png";
 
 function ProfilePage() {
     const [hasLoaded, setHasLoaded] = useState(false);
+    const [profilePosts, setProfilePosts] = useState({ results: [] });
+
     const currentUser = useCurrentUser();
     const { id } = useParams();
-    const setProfileData = useSetProfileData();
+
+    const { setProfileData, handleFollow } = useSetProfileData();
     const { pageProfile } = useProfileData();
+
     const [profile] = pageProfile.results;
     const is_owner = currentUser?.username === profile?.owner;
-    const [profilePosts, setProfilePosts] = useState({ results: [] });
+    
 
     useEffect(() => {
         const fetchData = async () => {
@@ -91,7 +95,7 @@ function ProfilePage() {
                         ) : (
                             <Button
                                 className={`${btnStyles.Button} ${btnStyles.Black}`}
-                                onClick={() => { }}
+                                onClick={() => handleFollow(profile)}
                             >
                                 follow
                             </Button>
